4c3401518cb384e9c2aa23a1f173289cf8fee497
rakotojaona <53557693+nambhine1@users.noreply.github.com>
2020-03-04 17:00:49 +0100

    Update pythonImageEnhanc.py

--- pythonImageEnhanc.py
+++ pythonImageEnhanc.py
@@ -1,14 +1,15 @@
 import cv2;
 import math;
 import numpy as np;
-
+# im : input image
+# Nous devrons trouver la valeur de input image
 def DarkChannel(im,sz):
     b,g,r = cv2.split(im)
     dc = cv2.min(cv2.min(r,g),b);
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(sz,sz))
     dark = cv2.erode(dc,kernel)
     return dark
-
+# ATMLIGHT : Atmosphere light
 def AtmLight(im,dark):
     [h,w] = im.shape[:2]
     imsz = h*w
@@ -25,7 +26,7 @@ def AtmLight(im,dark):
 
     A = atmsum / numpx;
     return A
-
+#medium transmission est en fonction de dark channel de input image
 def TransmissionEstimate(im,A,sz):
     omega = 0.95;
     im3 = np.empty(im.shape,im.dtype);
@@ -35,35 +36,8 @@ def TransmissionEstimate(im,A,sz):
 
     transmission = 1 - omega*DarkChannel(im3,sz);
     return transmission
-
-def Guidedfilter(im,p,r,eps):
-    mean_I = cv2.boxFilter(im,cv2.CV_64F,(r,r));
-    mean_p = cv2.boxFilter(p, cv2.CV_64F,(r,r));
-    mean_Ip = cv2.boxFilter(im*p,cv2.CV_64F,(r,r));
-    cov_Ip = mean_Ip - mean_I*mean_p;
-
-    mean_II = cv2.boxFilter(im*im,cv2.CV_64F,(r,r));
-    var_I   = mean_II - mean_I*mean_I;
-
-    a = cov_Ip/(var_I + eps);
-    b = mean_p - a*mean_I;
-
-    mean_a = cv2.boxFilter(a,cv2.CV_64F,(r,r));
-    mean_b = cv2.boxFilter(b,cv2.CV_64F,(r,r));
-
-    q = mean_a*im + mean_b;
-    return q;
-
-def TransmissionRefine(im,et):
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
-    gray = np.float64(gray)/255;
-    r = 60;
-    eps = 0.0001;
-    t = Guidedfilter(gray,et,r,eps);
-
-    return t;
-
-def Recover(im,t,A,tx = 0.1):
+# Apres obtenire la valeur maticielle de te ,A ont peut calculer output image
+def Recover(im,te,A,tx = 0.1):
     res = np.empty(im.shape,im.dtype);
     t = cv2.max(t,tx);
 
@@ -87,7 +61,7 @@ t = TransmissionRefine(src,te);
 J = Recover(I,t,A,0.1);
 
 cv2.imshow("dark",dark);
-cv2.imshow("Medium transmission t",t);
+cv2.imshow("Medium transmission t",te);
 cv2.imshow('Input Image I',src);
 cv2.imshow('Enhanced Image J ',J);
 cv2.imwrite("/outputimage.png",J*255);

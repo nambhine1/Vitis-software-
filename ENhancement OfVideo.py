7195cc4222ef07997fcd8255bc4da1e291848f02
rakotojaona <53557693+nambhine1@users.noreply.github.com>
2020-03-04 17:03:36 +0100

    Update ENhancement OfVideo.py

--- ENhancement OfVideo.py
+++ ENhancement OfVideo.py
@@ -36,34 +36,7 @@ def TransmissionEstimate(im,A,sz):
     transmission = 1 - omega*DarkChannel(im3,sz);
     return transmission
 
-def Guidedfilter(im,p,r,eps):
-    mean_I = cv2.boxFilter(im,cv2.CV_64F,(r,r));
-    mean_p = cv2.boxFilter(p, cv2.CV_64F,(r,r));
-    mean_Ip = cv2.boxFilter(im*p,cv2.CV_64F,(r,r));
-    cov_Ip = mean_Ip - mean_I*mean_p;
-
-    mean_II = cv2.boxFilter(im*im,cv2.CV_64F,(r,r));
-    var_I   = mean_II - mean_I*mean_I;
-
-    a = cov_Ip/(var_I + eps);
-    b = mean_p - a*mean_I;
-
-    mean_a = cv2.boxFilter(a,cv2.CV_64F,(r,r));
-    mean_b = cv2.boxFilter(b,cv2.CV_64F,(r,r));
-
-    q = mean_a*im + mean_b;
-    return q;
-
-def TransmissionRefine(im,et):
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
-    gray = np.float64(gray)/255;
-    r = 60;
-    eps = 0.0001;
-    t = Guidedfilter(gray,et,r,eps);
-
-    return t;
-
-def Recover(im,t,A,tx = 0.1):
+def Recover(im,te,A,tx = 0.1):
     res = np.empty(im.shape,im.dtype);
     t = cv2.max(t,tx);
 
@@ -85,8 +58,7 @@ while(True):
     dark = DarkChannel(I,15);
     A = AtmLight(I,dark);
     te = TransmissionEstimate(I,A,15);
-    t = TransmissionRefine(src,te);
-    J = Recover(I,t,A,0.1);
+    J = Recover(I,te,A,0.1);
    
     # Our operations on the frame come here
 
